0d287cb031e50250aa2042ef1c148be1f6891768
Louis <6723574+louisgv@users.noreply.github.com>
2022-06-02 20:35:14 -0400

    Initiate create-plasmo-ext

---
+++ cli/create-plasmo-ext/src/index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import { argv, exit } from "process"
+
+import { ErrorMessage } from "@plasmo/constants"
+import { aLog, eLog, exitCountDown, iLog } from "@plasmo/utils"
+
+async function main() {
+  try {
+    // In case someone pasted an essay into the cli
+    if (argv.length > 10) {
+      throw new Error(ErrorMessage.TooManyArg)
+    }
+
+    iLog("HELLO WORLD 147")
+  } catch (e) {
+    eLog((e as Error).message || ErrorMessage.Unknown)
+    aLog(e.stack)
+    await exitCountDown(3)
+    exit(1)
+  }
+}
+
+main()
+
+process.on("SIGINT", () => exit(0))
+process.on("SIGTERM", () => exit(0))
