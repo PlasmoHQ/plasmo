011d682d88b730b69f7f5f104654d39f848b328a
Louis <6723574+louisgv@users.noreply.github.com>
2022-06-14 19:45:29 -0400

    Use require.resolve

--- cli/plasmo/src/commands/build.ts
+++ cli/plasmo/src/commands/build.ts
@@ -45,13 +45,13 @@ async function build() {
   const bundler = new Parcel({
     cacheDir: resolve(cacheDirectory, "parcel"),
     entries: commonPath.entryManifestPath,
-    config: "@plasmohq/parcel-config",
+    config: require.resolve("@plasmohq/parcel-config"),
     mode: "production",
     shouldAutoInstall: true,
     shouldDisableCache: true,
     defaultTargetOptions: {
       shouldOptimize: true,
-      shouldScopeHoist: false,
+      shouldScopeHoist: true,
       sourceMaps: false,
       engines: {
         browsers: ["last 1 Chrome version"]
