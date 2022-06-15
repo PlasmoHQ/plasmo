012b7325ed3d1bf5f9a46531190426c5d08a078d
Louis <6723574+louisgv@users.noreply.github.com>
2022-06-14 23:08:57 -0400

    Bump dependencies

--- cli/plasmo/src/features/extension-devtools/package-file.ts
+++ cli/plasmo/src/features/extension-devtools/package-file.ts
@@ -24,16 +24,16 @@ export const generatePackage = ({
     },
     dependencies: {
       plasmo: "latest",
-      react: "18.1.0",
-      "react-dom": "18.1.0"
+      react: "18.2.0",
+      "react-dom": "18.2.0"
     } as Record<string, string>,
     devDependencies: {
       "@trivago/prettier-plugin-sort-imports": "3.2.0",
       "@types/chrome": "0.0.190",
-      "@types/node": "17.0.42",
+      "@types/node": "17.0.43",
       "@types/react": "18.0.12",
       "@types/react-dom": "18.0.5",
-      prettier: "2.6.2",
+      prettier: "2.7.0",
       typescript: "4.7.3"
     },
     manifest: {
