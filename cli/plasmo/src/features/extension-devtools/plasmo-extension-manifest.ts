3d96a41d093dd4d12cd17828946a82d7b2f8aacf
Stefan Aleksic <stefan@plasmo.com>
2022-06-15 01:48:06 +0100

    Remove unnecessary curly braces

--- cli/plasmo/src/features/extension-devtools/plasmo-extension-manifest.ts
+++ cli/plasmo/src/features/extension-devtools/plasmo-extension-manifest.ts
@@ -71,9 +71,9 @@ export class PlasmoExtensionManifest {
     this.#data.description = this.#packageData.description
     this.#data.author = this.#packageData.author
 
-    this.#data.permissions = autoPermissionList.filter((p) => {
-      return `@plasmohq/${p}` in this.#packageData.dependencies
-    })
+    this.#data.permissions = autoPermissionList.filter(
+      (p) => `@plasmohq/${p}` in this.#packageData.dependencies
+    )
 
     if (this.#data.permissions.length === 0) {
       delete this.#data.permissions
