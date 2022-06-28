c8ab8cce5f3aed1ad74668314c6a3f59088bde20
Louis <6723574+louisgv@users.noreply.github.com>
2022-06-27 22:44:03 -0400

    fix: implement own ast parser to reduce dep

---
+++ cli/plasmo/src/features/extension-devtools/parse-ast.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) Plasmo Corp, foss@plasmo.com, MIT Licensed
+ * ---
+ * Adapted from https://github.com/dword-design/ts-ast-to-literal/blob/master/src/index.js
+ * Copyright (c) Sebastian Landwehr info@sebastianlandwehr.com, MIT licensed
+ */
+import type {
+  ArrayLiteralExpression,
+  Identifier,
+  LiteralExpression,
+  Node,
+  ObjectLiteralExpression,
+  PropertyAssignment
+} from "typescript"
+import { SyntaxKind } from "typescript"
+
+export const parseAst = (node: Node) => {
+  switch (node.kind) {
+    case SyntaxKind.StringLiteral:
+      return (node as LiteralExpression).text
+    case SyntaxKind.TrueKeyword:
+      return true
+    case SyntaxKind.FalseKeyword:
+      return false
+    case SyntaxKind.NullKeyword:
+      return null
+    case SyntaxKind.NumericLiteral:
+      return parseFloat((node as LiteralExpression).text)
+    case SyntaxKind.ArrayLiteralExpression:
+      return (node as ArrayLiteralExpression).elements
+        .filter((node) => node.kind !== SyntaxKind.SpreadElement)
+        .map(parseAst)
+    case SyntaxKind.ObjectLiteralExpression:
+      return (node as ObjectLiteralExpression).properties
+        .filter(
+          (property) =>
+            property.kind === SyntaxKind.PropertyAssignment &&
+            (property.name.kind === SyntaxKind.Identifier ||
+              property.name.kind === SyntaxKind.StringLiteral)
+        )
+        .map((property: PropertyAssignment) => [
+          (property.name as Identifier).escapedText ||
+            (property.name as LiteralExpression).text,
+          parseAst(property.initializer)
+        ])
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
+    default:
+      return undefined
+  }
+}
