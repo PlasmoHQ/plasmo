71c95bc3e68400a4461fa853e147835721287df7
Neo Tan <neotan12@hotmail.com>
2022-08-27 17:31:08 -0400

    fix: max out csui z-index (#157)
    * remove css important
    
    Signed-off-by: L <6723574+louisgv@users.noreply.github.com>
    Co-authored-by: L <6723574+louisgv@users.noreply.github.com>

--- cli/plasmo/templates/static/svelte3/content-script-ui-mount.ts
+++ cli/plasmo/templates/static/svelte3/content-script-ui-mount.ts
@@ -49,7 +49,7 @@ async function createShadowContainer() {
   container.id = "plasmo-shadow-container"
 
   container.style.cssText = `
-    z-index: 1;
+    z-index: 2147483647;
     position: absolute;
   `
 
