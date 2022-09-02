31d76eb1166016a14c321852a325563ef4ca987e
Carlos Gomez <carlos.gm@celtiberian.es>
2022-09-02 09:37:30 +0200

    Added explicit React import to react18 template

--- cli/plasmo/templates/static/react18/index.tsx
+++ cli/plasmo/templates/static/react18/index.tsx
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import * as Component from "__plasmo_import_module__"
+import React from "react"
 import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
