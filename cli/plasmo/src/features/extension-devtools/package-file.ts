a9adfc29c43ad62fa0afe63f67c2b3c3b6c82b4d
Louis <6723574+louisgv@users.noreply.github.com>
2022-09-05 09:26:46 -0400

    chore: bump package deps

--- cli/plasmo/src/features/extension-devtools/package-file.ts
+++ cli/plasmo/src/features/extension-devtools/package-file.ts
@@ -30,12 +30,12 @@ export const generatePackage = ({
     } as Record<string, string>,
     devDependencies: {
       "@plasmohq/prettier-plugin-sort-imports": "1.2.0",
-      "@types/chrome": "0.0.193",
-      "@types/node": "18.6.4",
-      "@types/react": "18.0.17",
+      "@types/chrome": "0.0.196",
+      "@types/node": "18.7.14",
+      "@types/react": "18.0.18",
       "@types/react-dom": "18.0.6",
       prettier: "2.7.1",
-      typescript: "4.7.4"
+      typescript: "4.8.2"
     } as Record<string, string>,
     manifest: {
       // permissions: [] as ValidManifestPermission[],
