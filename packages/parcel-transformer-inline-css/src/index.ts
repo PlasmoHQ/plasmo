e435f9496c6286569ba197c5d5e5c290e1cd93cf
Louis <6723574+louisgv@users.noreply.github.com>
2022-09-14 09:49:38 -0400

    Implement custom inline for css module

---
+++ packages/parcel-transformer-inline-css/src/index.ts
@@ -0,0 +1,57 @@
+import { transform } from "@parcel/css"
+import { Transformer } from "@parcel/plugin"
+import { remapSourceLocation } from "@parcel/utils"
+import path from "path"
+
+export default new Transformer({
+  async transform({ asset, options }) {
+    // Normalize the asset's environment so that properties that only affect JS don't cause CSS to be duplicated.
+    // For example, with ESModule and CommonJS targets, only a single shared CSS bundle should be produced.
+    let [code, originalMap] = await Promise.all([
+      asset.getBuffer(),
+      asset.getMap()
+    ])
+
+    let res = transform({
+      filename: path.relative(options.projectRoot, asset.filePath),
+      code,
+      cssModules: true,
+      analyzeDependencies: asset.meta.hasDependencies !== false,
+      sourceMap: !!asset.env.sourceMap
+    })
+
+    asset.setBuffer(res.code)
+
+    if (res.dependencies) {
+      for (let dep of res.dependencies) {
+        let loc = dep.loc
+
+        if (originalMap) {
+          loc = remapSourceLocation(loc, originalMap)
+        }
+
+        if (dep.type === "import" && !res.exports) {
+          asset.addDependency({
+            specifier: dep.url,
+            specifierType: "url",
+            loc,
+            meta: {
+              // For the glob resolver to distinguish between `@import` and other URL dependencies.
+              isCSSImport: true,
+              media: dep.media
+            }
+          })
+        } else if (dep.type === "url") {
+          asset.addURLDependency(dep.url, {
+            loc,
+            meta: {
+              placeholder: dep.placeholder
+            }
+          })
+        }
+      }
+    }
+
+    return [asset]
+  }
+})
