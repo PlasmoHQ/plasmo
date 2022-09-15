7bb7429544276913bc07705ec65f2575474aebc4
L <6723574+louisgv@users.noreply.github.com>
2022-09-15 09:23:37 -0700

    feat: css-modules inlining for CSUI (#207)
    * Implement custom inline for css module
    
    * remove unused deps
    
    * do some minor refactor

---
+++ packages/parcel-transformer-inline-css/src/index.ts
@@ -0,0 +1,55 @@
+import { transform } from "@parcel/css"
+import { Transformer } from "@parcel/plugin"
+import { remapSourceLocation } from "@parcel/utils"
+import { relative } from "path"
+
+export default new Transformer({
+  async transform({ asset, options }) {
+    // Normalize the asset's environment so that properties that only affect JS don't cause CSS to be duplicated.
+    // For example, with ESModule and CommonJS targets, only a single shared CSS bundle should be produced.
+    const [code, originalMap] = await Promise.all([
+      asset.getBuffer(),
+      asset.getMap()
+    ])
+
+    const res = transform({
+      filename: relative(options.projectRoot, asset.filePath),
+      code,
+      cssModules: true,
+      analyzeDependencies: asset.meta.hasDependencies !== false,
+      sourceMap: !!asset.env.sourceMap
+    })
+
+    asset.setBuffer(res.code)
+
+    if (res.dependencies) {
+      for (let dep of res.dependencies) {
+        const loc = !originalMap
+          ? dep.loc
+          : remapSourceLocation(dep.loc, originalMap)
+
+        if (dep.type === "import" && !res.exports) {
+          asset.addDependency({
+            specifier: dep.url,
+            specifierType: "url",
+            loc,
+            meta: {
+              // For the glob resolver to distinguish between `@import` and other URL dependencies.
+              isCSSImport: true,
+              media: dep.media
+            }
+          })
+        } else if (dep.type === "url") {
+          asset.addURLDependency(dep.url, {
+            loc,
+            meta: {
+              placeholder: dep.placeholder
+            }
+          })
+        }
+      }
+    }
+
+    return [asset]
+  }
+})
