eaea7fe40961aa1524edf25bb9edeb445cf05287
Louis <6723574+louisgv@users.noreply.github.com>
2022-09-23 18:32:34 -0400

    feat: reduce HMR warning, only native WS message error are shown. #222

---
+++ packages/parcel-transformer-manifest/src/handle-tabs.ts
@@ -0,0 +1,37 @@
+import { checkMV2, state } from "./state"
+
+export async function handleTabs() {
+  const { program, filePath, ptrs, asset } = state
+
+  const isMV2 = checkMV2(program)
+
+  // const browserActionName = isMV2 ? "browser_action" : "action"
+
+  // const browserAction = isMV2 ? program.browser_action : program.action
+
+  // if (!browserAction) {
+  //   return
+  // }
+
+  // if (browserAction.theme_icons) {
+  //   browserAction.theme_icons = browserAction.theme_icons.map(
+  //     (themeIcon, themeIndex) => {
+  //       for (const k of ["light", "dark"]) {
+  //         const loc = getJSONSourceLocation(
+  //           ptrs[`/${browserActionName}/theme_icons/${themeIndex}/${k}`],
+  //           "value"
+  //         )
+
+  //         themeIcon[k] = asset.addURLDependency(themeIcon[k], {
+  //           loc: {
+  //             ...loc,
+  //             filePath
+  //           }
+  //         })
+  //       }
+
+  //       return themeIcon
+  //     }
+  //   )
+  // }
+}
