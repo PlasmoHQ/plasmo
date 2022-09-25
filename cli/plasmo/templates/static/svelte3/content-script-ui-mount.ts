2106b7b1f76640b5141294bf9378a771483ef3fc
Louis <6723574+louisgv@users.noreply.github.com>
2022-09-24 20:09:35 -0400

    align the csui mount for all ui templates

--- cli/plasmo/templates/static/svelte3/content-script-ui-mount.ts
+++ cli/plasmo/templates/static/svelte3/content-script-ui-mount.ts
@@ -2,10 +2,23 @@
 // @ts-ignore
 import * as RawMount from "__plasmo_mount_content_script__"
 
-import type { PlasmoCSUI } from "../../../src/type"
+import type { PlasmoCSUI, PlasmoCSUIMountState } from "../../../src/type"
 
 // Escape parcel's static analyzer
 const Mount = RawMount as PlasmoCSUI
+
+const mountState: PlasmoCSUIMountState = {
+  document: document || window.document,
+  observer: null,
+  shadowHost: null,
+  // cached anchors:
+  inlineAnchor: null
+}
+
+const isMounted = (el: Element | null) =>
+  el?.id
+    ? !!document.getElementById(el.id)
+    : el?.getRootNode({ composed: true }) === mountState.document
 
 const createMountContainer = () => {
   const container = document.createElement("div")
@@ -52,9 +65,11 @@ async function createShadowContainer() {
 
   container.id = "plasmo-shadow-container"
 
+  const isInline = !!mountState.inlineAnchor
+
   container.style.cssText = `
     z-index: 2147483647;
-    position: absolute;
+    position: ${isInline ? "relative" : "absolute"};
   `
 
   const shadowHost = document.createElement("div")
@@ -63,8 +78,20 @@ async function createShadowContainer() {
     shadowHost.id = await Mount.getShadowHostId()
   }
 
-  const shadowRoot = shadowHost.attachShadow({ mode: "open" })
-  document.body.insertAdjacentElement("beforebegin", shadowHost)
+  const shadowRoot =
+    typeof Mount.createShadowRoot === "function"
+      ? await Mount.createShadowRoot(shadowHost)
+      : shadowHost.attachShadow({ mode: "open" })
+
+  mountState.shadowHost = shadowHost
+
+  if (typeof Mount.mountShadowHost === "function") {
+    await Mount.mountShadowHost(mountState)
+  } else if (isInline) {
+    mountState.inlineAnchor?.insertAdjacentElement("afterend", shadowHost)
+  } else {
+    document.body.insertAdjacentElement("beforebegin", shadowHost)
+  }
 
   if (typeof Mount.getStyle === "function") {
     shadowRoot.appendChild(await Mount.getStyle())
@@ -74,22 +101,46 @@ async function createShadowContainer() {
   return container
 }
 
-new MutationObserver(async (_, observer) => {
-  const rootContainer =
-    typeof Mount.getRootContainer === "function"
-      ? await Mount.getRootContainer()
-      : await createShadowContainer()
+const createRootContainer =
+  typeof Mount.getRootContainer === "function"
+    ? Mount.getRootContainer
+    : createShadowContainer
 
-  if (!rootContainer) {
-    return
-  }
-
-  observer.disconnect()
-
+const render = async () => {
+  const rootContainer = await createRootContainer()
   const mountPoint = createMountContainer()
   rootContainer.appendChild(mountPoint)
-
   new Mount.default({
     target: mountPoint
   })
-}).observe(document, { childList: true, subtree: true })
+}
+
+const startObserver = () => {
+  mountState.observer = new MutationObserver(() => {
+    if (isMounted(mountState.shadowHost)) {
+      return
+    }
+    const inlineAnchor = Mount.getInlineAnchor()
+    if (!inlineAnchor) {
+      return
+    }
+
+    mountState.inlineAnchor = inlineAnchor
+    render()
+  })
+
+  mountState.observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  })
+}
+
+window.addEventListener("load", () => {
+  if (typeof Mount.render === "function") {
+    Mount.render(createRootContainer, createMountContainer)
+  } else if (typeof Mount.getInlineAnchor === "function") {
+    startObserver()
+  } else {
+    render()
+  }
+})
