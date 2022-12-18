61b913241cf5b0cf66294a74d6146c79f963e09f
Louis <6723574+louisgv@users.noreply.github.com>
2022-12-17 21:32:00 -0400

    fix: polyfill bundling for browser

---
+++ packages/parcel-resolver/index.mjs
@@ -0,0 +1,28 @@
+import { build } from "esbuild"
+import glob from "tiny-glob"
+
+async function main() {
+  const entryPoints = await glob("./src/polyfills/**/*.ts", {
+    filesOnly: true
+  })
+
+  await build({
+    entryPoints,
+
+    bundle: true,
+    minify: true,
+    sourcemap: false,
+
+    platform: "browser",
+    target: ["chrome74", "safari11"],
+
+    alias: {
+      stream: "stream-browserify",
+      http: "stream-http"
+    },
+
+    outdir: "dist/polyfills"
+  })
+}
+
+main()
