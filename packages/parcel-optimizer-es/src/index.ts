24e54b93aace36242a3d110b337e235f7bc28f4a
Louis <6723574+louisgv@users.noreply.github.com>
2023-01-10 20:00:28 -0400

    switch to esbuild for optimizer

--- packages/parcel-optimizer-es/src/index.ts
+++ packages/parcel-optimizer-es/src/index.ts
@@ -2,46 +2,21 @@
  * Copyright (c) 2022 Plasmo Corp. <foss@plasmo.com> (https://www.plasmo.com) and contributors
  * MIT License
  *
- * Based on: https://github.com/parcel-bundler/parcel/blob/eed1e2ca5f4bde73975b01f78464af6a75258257/packages/optimizers/terser/src/TerserOptimizer.js
+ * Based on: https://github.com/parcel-bundler/parcel/blob/723e8443757cfb12a1a3af8180f0d923e666e1aa/packages/optimizers/esbuild/src/ESBuildOptimizer.js
  * MIT License
  */
 
 import { Optimizer } from "@parcel/plugin"
 import SourceMap from "@parcel/source-map"
-import { blobToString } from "@parcel/utils"
-import nullthrows from "nullthrows"
-import { extname, join, relative } from "path"
-import { MinifyOptions, minify } from "terser"
+import { blobToString, normalizePath } from "@parcel/utils"
+import { transform } from "esbuild"
+import { join, relative } from "path"
 
 import { toUtf8 } from "./to-utf8"
 
 export default new Optimizer({
-  async loadConfig({ config, options }) {
-    const userConfig = await config.getConfigFrom<MinifyOptions>(
-      join(options.projectRoot, "index"),
-      [".terserrc", ".terserrc.js", ".terserrc.cjs"]
-    )
-
-    if (userConfig) {
-      const isJs = extname(userConfig.filePath) === ".js"
-
-      if (isJs) {
-        config.invalidateOnStartup()
-      }
-    }
-
-    return userConfig?.contents || {}
-  },
-
-  async optimize({
-    contents,
-    map,
-    bundle,
-    config: { format = {}, ...userConfig },
-    options,
-    getSourceMapReference
-  }) {
-    const code = (await blobToString(contents)) as string
+  async optimize({ contents, map, bundle, options, getSourceMapReference }) {
+    let code = (await blobToString(contents)) as string
 
     if (!bundle.env.shouldOptimize) {
       return {
@@ -50,51 +25,42 @@ export default new Optimizer({
       }
     }
 
-    const originalMap = map ? await map.stringify({}) : null
+    const relativeBundlePath = relative(
+      options.projectRoot,
+      join(bundle.target.distDir, bundle.name)
+    )
 
-    console.log(`>>>>>>>>>>> TERSER CONFIG: ${bundle.env.outputFormat}`)
-
-    const config: MinifyOptions = {
-      // format: {
-      //   ...format,
-      //   ascii_only: true
-      // },
-      sourceMap: bundle.env.sourceMap
-        ? ({
-            filename: relative(
-              options.projectRoot,
-              join(bundle.target.distDir, bundle.name)
-            ),
-            asObject: true,
-            content: originalMap
-          } as any)
-        : false,
-      toplevel:
-        bundle.env.outputFormat === "esmodule" ||
-        bundle.env.outputFormat === "commonjs",
-      module: bundle.env.outputFormat === "esmodule",
-      ...userConfig
+    if (map) {
+      let vlqMappings = await map.stringify({
+        file: normalizePath(relativeBundlePath + ".map"),
+        format: "inline"
+      })
+      code += `\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,${vlqMappings}`
     }
 
-    const result = await minify(code, config)
+    let { code: esOutput, map: esSourceMap } = await transform(code, {
+      sourcemap: "external",
+      sourcefile: relativeBundlePath,
+      minify: true,
+      treeShaking: true,
+      format: "esm"
+    })
 
-    let minifiedContents: string = nullthrows(result.code)
+    let sourcemap = null
+    if (esSourceMap) {
+      sourcemap = new SourceMap(options.projectRoot)
+      let parsedMap = JSON.parse(esSourceMap)
+      sourcemap.addVLQMap(parsedMap)
 
-    let sourceMap = null
-    let resultMap = result.map
-
-    if (resultMap && typeof resultMap !== "string") {
-      sourceMap = new SourceMap(options.projectRoot)
-      sourceMap.addVLQMap(resultMap)
-      const sourcemapReference = await getSourceMapReference(sourceMap)
+      let sourcemapReference = await getSourceMapReference(sourcemap)
       if (sourcemapReference) {
-        minifiedContents += `\n//# sourceMappingURL=${sourcemapReference}\n`
+        esOutput += `\n//# sourceMappingURL=${sourcemapReference}\n`
       }
     }
 
     return {
-      contents: minifiedContents,
-      map: sourceMap
+      contents: esOutput,
+      map: sourcemap || map
     }
   }
 })
