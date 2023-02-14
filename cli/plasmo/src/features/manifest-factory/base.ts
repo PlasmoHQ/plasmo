6ac07c65b99797b3f1264ebc10761dd1c57dc1f7
Louis <6723574+louisgv@users.noreply.github.com>
2023-02-14 00:40:01 -0400

    fix: page path aligning with new source dir

--- cli/plasmo/src/features/manifest-factory/base.ts
+++ cli/plasmo/src/features/manifest-factory/base.ts
@@ -271,8 +271,6 @@ export abstract class PlasmoManifest<T extends ExtensionManifest = any> {
       this.browser,
       this.uiExts
     )
-
-    vLog(this.projectPath)
   }
 
   abstract togglePopup: (enable?: boolean) => this
@@ -424,11 +422,9 @@ export abstract class PlasmoManifest<T extends ExtensionManifest = any> {
     }
 
     if (enable) {
-      const scriptPath = relative(this.commonPath.dotPlasmoDirectory, path)
+      const scriptPath = relative(this.commonPath.sourceDirectory, path)
 
-      const modulePath = join("lab", scriptPath).replace(/(^src)[\\/]/, "")
-
-      const parsedModulePath = parse(modulePath)
+      const parsedModulePath = parse(scriptPath)
 
       const { wereFilesWritten } = await this.scaffolder.createPageMount(
         parsedModulePath
