8e0e53982418d9792ca630ae020d51accc1d44fb
L <6723574+louisgv@users.noreply.github.com>
2023-02-26 20:30:23 -0400

    fix(messaging): use promise API (#462)

--- packages/messaging/src/index.ts
+++ packages/messaging/src/index.ts
@@ -15,39 +15,25 @@ export type {
  * Should only be called from CS or Ext Pages
  * TODO: Add a framework runtime check, using a global varaible
  */
-export const sendToBackground: PlasmoMessaging.SendFx = (req) =>
-  new Promise((resolve, reject) => {
-    if (!chrome?.runtime) {
-      throw new Error("chrome.runtime is not available")
-    }
-    chrome.runtime.sendMessage(req, (res) => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError)
-      } else {
-        resolve(res)
-      }
-    })
-  })
+export const sendToBackground: PlasmoMessaging.SendFx = async (req) => {
+  if (!chrome?.runtime) {
+    throw new Error("chrome.runtime is not available")
+  }
+  return chrome.runtime.sendMessage(req)
+}
 
 /**
  * Send to CS from Ext pages or BGSW, default to active tab if no tabId is provided in the request
  */
-export const sendToContentScript: PlasmoMessaging.SendFx = (req) =>
-  new Promise(async (resolve, reject) => {
-    if (!chrome?.tabs) {
-      throw new Error("chrome.tabs is not available")
-    }
-    const tabId =
-      typeof req.tabId === "number" ? req.tabId : (await getActiveTab()).id
+export const sendToContentScript: PlasmoMessaging.SendFx = async (req) => {
+  if (!chrome?.tabs) {
+    throw new Error("chrome.tabs is not available")
+  }
+  const tabId =
+    typeof req.tabId === "number" ? req.tabId : (await getActiveTab()).id
 
-    chrome.tabs.sendMessage(tabId, req, (res) => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError)
-      } else {
-        resolve(res)
-      }
-    })
-  })
+  return chrome.tabs.sendMessage(tabId, req)
+}
 
 /**
  * @deprecated Renamed to `sendToContentScript`
