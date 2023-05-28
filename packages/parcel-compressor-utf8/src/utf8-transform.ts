f530efa46be2f4e07c2632ce027b3fcfa76772ef
Louis <6723574+louisgv@users.noreply.github.com>
2023-05-28 16:36:24 -0400

    chore: bump deps

--- packages/parcel-compressor-utf8/src/utf8-transform.ts
+++ packages/parcel-compressor-utf8/src/utf8-transform.ts
@@ -2,7 +2,6 @@
  * Copyright (c) 2023 Plasmo Corp. <foss@plasmo.com> (https://www.plasmo.com) and contributors
  * MIT License
  */
-
 import { Transform, type TransformCallback } from "stream"
 import { StringDecoder } from "string_decoder"
 
@@ -10,17 +9,22 @@ class Utf8Transform extends Transform {
   private decoder = new StringDecoder("utf8")
 
   _transform(chunk: Buffer, _: BufferEncoding, callback: TransformCallback) {
-    const str = this.decoder.write(chunk)
-    callback(null, str)
+    callback(null, this.transformChunk(this.decoder.write(chunk)))
   }
 
   _flush(callback: TransformCallback) {
     const remainder = this.decoder.end()
-    if (remainder) {
-      callback(null, remainder)
-    } else {
-      callback()
-    }
+    remainder ? callback(null, this.transformChunk(remainder)) : callback()
+  }
+
+  private transformChunk(chunk: string): string {
+    return Array.from(chunk)
+      .map((ch) =>
+        ch.charCodeAt(0) <= 0x7f
+          ? ch
+          : "\\u" + ("0000" + ch.charCodeAt(0).toString(16)).slice(-4)
+      )
+      .join("")
   }
 }
 
