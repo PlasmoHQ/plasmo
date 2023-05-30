720aebede0af46a31e13230817da6a10c781f95c
Louis <6723574+louisgv@users.noreply.github.com>
2023-05-30 19:14:32 -0400

    chore: reuse tsconfig

--- packages/persistent/src/background.ts
+++ packages/persistent/src/background.ts
@@ -1,6 +1,6 @@
 export const keepAlive = () => {
   const extRuntime = (globalThis.browser.runtime ||
-    globalThis.chrome.runtime) as chrome.runtime
+    globalThis.chrome.runtime) as typeof chrome.runtime
   const _keepAlive = () => setInterval(extRuntime.getPlatformInfo, 20e3)
   extRuntime.onStartup.addListener(_keepAlive)
   _keepAlive()
