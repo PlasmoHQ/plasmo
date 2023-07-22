ea81a487ed829226c20b997b762e4c9ea267d0f4
Matthew Schroeter <schroeter.m83@gmail.com>
2023-07-21 22:45:18 -0500

    defined __VUE_OPTIONS_API__ and __VUE_PROD_DEVTOOLS__ in the static vue3 template to eliminate console warning

--- cli/plasmo/templates/static/vue3/index.ts
+++ cli/plasmo/templates/static/vue3/index.ts
@@ -3,6 +3,13 @@ import { createApp } from "vue"
 
 import * as Component from "__plasmo_import_module__"
 
+__VUE_OPTIONS_API__ = true
+if(process.env.NODE_ENV !== 'production') {
+  __VUE_PROD_DEVTOOLS__ = true
+} else {
+  __VUE_PROD_DEVTOOLS__ = false
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const app = createApp(Component.default)
   Component.default.prepare?.(app)
