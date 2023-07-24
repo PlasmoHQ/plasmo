f8f665c7e470bcdf2ac012389251bc418db145d1
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-07-24 01:15:57 -0700

    fix flag

--- cli/plasmo/templates/static/common/vue.ts
+++ cli/plasmo/templates/static/common/vue.ts
@@ -1,5 +1,2 @@
-if (process.env.NODE_ENV !== "production") {
-  globalThis.__VUE_OPTIONS_API__ = true
-} else {
-  globalThis.__VUE_OPTIONS_API__ = false
-}
+globalThis.__VUE_OPTIONS_API__ = true
+globalThis.__VUE_PROD_DEVTOOLS__ = process.env.NODE_ENV !== "production"
