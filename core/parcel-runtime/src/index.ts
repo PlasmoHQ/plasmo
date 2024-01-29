f9e9c0149b4370acc7218f99f904e4a4faab1202
Deavial <victoria.french@cinecove.com>
2024-01-29 16:31:21 -0600

    fix: Allow React to be detected in NPM Workspaces (#864)
    * Allow React to be detected in mono repo projects
    
    React is detected by looking for a react devDependency or dependency. NPM Workspaces (mono repos) do not define these in the project package.json. The mono repo will fail if they are defined. The mono repo root has the dependencies listed.
    
    When looking for React in this type of environment, the peerDependencies should be inspected instead.
    
    * fix for react not loading
    
    ---------
    
    Co-authored-by: L <6723574+louisgv@users.noreply.github.com>

--- core/parcel-runtime/src/index.ts
+++ core/parcel-runtime/src/index.ts
@@ -27,6 +27,7 @@ export default new Runtime({
       .getConfigFrom<{
         dependencies: Record<string, string>
         devDependencies: Record<string, string>
+        peerDependencies: Record<string, string>
       }>(
         join(process.env.PLASMO_PROJECT_DIR, "lab"), // parcel only look up
         ["package.json"],
@@ -36,7 +37,8 @@ export default new Runtime({
       )
       .then((cfg) => cfg?.contents)
 
-    const hasReact = !!pkg?.dependencies?.react || !!pkg?.devDependencies?.react
+    // npm workspaces mono repo's do not have dependencies or devDependencies (they are defined in a parent directory), fallback to peerDependencies
+    const hasReact = !!pkg?.dependencies?.react || !!pkg?.devDependencies?.react || !!pkg?.peerDependencies?.react
 
     return {
       hasReact
